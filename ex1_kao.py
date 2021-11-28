18a89435dcbb3134873086e71642ad7af643b3a1
Wataru KUNINO <git@bokunimo.net>
2021-11-28 14:34:18 +0900

    基本サンプル

---
+++ ex1_kao.py
@@ -0,0 +1,39 @@
+###############################################################################
+# Example 1 顔検出
+#                        for Sipeed M1n Module AI Development Kit based on K210
+#
+#                        Copyright (c) 2021 Wataru KUNINO https://bokunimo.net/
+###############################################################################
+# (参考文献) https://github.com/sipeed/MaixPy_scripts/
+#                        blob/master/machine_vision/face_find/demo_find_face.py
+
+import sensor, image
+import KPU as kpu
+
+# カメラ設定
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.set_hmirror(False)
+sensor.set_vflip(True)
+sensor.run(1)
+
+# AI設定
+anchors = (1.889, 2.525, 2.947, 3.941, 4.0, 5.366, 5.155, 6.923, 6.718, 9.01)
+task = None
+task = kpu.load(0x300000)
+kpu.init_yolo2(task, 0.3, 0.1, len(anchors)//2, anchors)
+
+while(not task is None):
+    img = sensor.snapshot()
+    objects = kpu.run_yolo2(task, img)
+    n = 0
+    if objects:
+        n = len(objects)
+        for obj in objects:
+            img.draw_rectangle(obj.rect())
+            img.draw_string(obj.x(), obj.y(), str(obj.value()))
+            print(obj.rect(), end=', ')
+        print()
+    img.draw_string(0, 200, 'n=' + str(n), scale=2)
+kpu.deinit(task)
---
+++ ex2_kao_uart.py
@@ -0,0 +1,51 @@
+###############################################################################
+# Example 1 顔検出 シリアル出力
+#                        for Sipeed M1n Module AI Development Kit based on K210
+#
+#                        Copyright (c) 2021 Wataru KUNINO https://bokunimo.net/
+###############################################################################
+# (参考文献) https://github.com/sipeed/MaixPy_scripts/
+#                        blob/master/machine_vision/face_find/demo_find_face.py
+
+import sensor, image
+import KPU as kpu
+from fpioa_manager import fm
+from machine import UART
+
+# シリアル設定
+fm.register(20, fm.fpioa.UART1_TX, force=True)
+uart = UART(UART.UART1, 115200, 8, 0, 1)
+uart.write('Hello!\n')
+
+# カメラ設定
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.set_hmirror(False)
+sensor.set_vflip(True)
+sensor.run(1)
+
+# AI設定
+anchors = (1.889, 2.525, 2.947, 3.941, 4.0, 5.366, 5.155, 6.923, 6.718, 9.01)
+task = None
+task = kpu.load(0x300000)
+kpu.init_yolo2(task, 0.3, 0.1, len(anchors)//2, anchors)
+
+while(not task is None):
+    img = sensor.snapshot()
+    objects = kpu.run_yolo2(task, img)
+    n = 0
+    s=''
+    if objects:
+        n = len(objects)
+        for obj in objects:
+            img.draw_rectangle(obj.rect())
+            img.draw_string(obj.x(), obj.y(), str(obj.value()))
+            if len(s) > 0:
+                s += ', '
+            s += str(obj.rect())
+        print(s)
+        uart.write(s + '\n')
+    img.draw_string(0, 200, 'n=' + str(n), scale=2)
+    img.draw_string(0, 225, s)
+kpu.deinit(task)
