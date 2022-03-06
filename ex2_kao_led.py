8179b154ecc8a7237a9781dac45fee63bf250fc7
Wataru KUNINO <git@bokunimo.net>
2022-03-06 16:21:44 +0900

    LED制御 出力先の変更、論理反転

--- ex2_kao_led.py
+++ ex2_kao_led.py
@@ -1,41 +1,50 @@
-###############################################################################
-# Example 2 AIカメラが顔検出したときに LED を点灯する
-#                        for Sipeed M1n Module AI Development Kit based on K210
+################################################################################
+# Example 2 AIカメラが顔検出したときに LED を点灯する for Sipeed Maix Dock
 #
-#                        Copyright (c) 2021 Wataru KUNINO https://bokunimo.net/
-###############################################################################
-# (参考文献) https://github.com/sipeed/MaixPy_scripts/
-#                        blob/master/machine_vision/face_find/demo_find_face.py
-#                        blob/master/hardware/demo_gpio_led.py
+#                   Copyright (c) 2021-2022 Wataru KUNINO https://bokunimo.net/
+################################################################################
 
-import sensor
-import KPU as kpu
-from Maix import GPIO
-from fpioa_manager import fm
 
-# LED設定
-fm.register(7, fm.fpioa.GPIO0, force=True)
-led = GPIO(GPIO.GPIO0, GPIO.OUT)
+import sensor, lcd                                  # カメラsensor,液晶lcdの組込
+import KPU as kpu                                   # AI演算ユニットKPUの組込
+from Maix import GPIO                               # GPIOモジュールの組込
+from fpioa_manager import fm                        # fmモジュールの組込
 
-# カメラ設定
-sensor.reset()
-sensor.set_pixformat(sensor.RGB565)
-sensor.set_framesize(sensor.QVGA)
-sensor.set_hmirror(True)
-sensor.set_vflip(False)
-sensor.run(1)
+fm.register(14, fm.fpioa.GPIO0, force=True)         # ポート14をGPIO0に割り当て
+led_r = GPIO(GPIO.GPIO0, GPIO.OUT)                  # GPIO0のオブジェクトled_r
+led_stat = ['On','Off']                             # led状態
 
-# AI設定
+lcd.init()                                          # LCDの初期化
+sensor.reset()                                      # カメラの初期化
+sensor.set_pixformat(sensor.RGB565)                 # 色設定(白黒時GRAYSCALE)
+sensor.set_framesize(sensor.QVGA)                   # 解像度設定(QVGA:320x240)
+sensor.set_vflip(True)                              # カメラ画像の上下反転設定
+sensor.set_hmirror(True)                            # カメラ画像の左右反転設定
+
+# AIの設定(face_model_at_0x300000.kfpkg用)
 anchors = (1.889, 2.525, 2.947, 3.941, 4.0, 5.366, 5.155, 6.923, 6.718, 9.01)
 task = kpu.load(0x300000)
 kpu.init_yolo2(task, 0.3, 0.1, len(anchors)//2, anchors)
 
-while(True):
-    img = sensor.snapshot()
-    objects = kpu.run_yolo2(task, img)
-    if objects:
-        img.draw_rectangle(objects[0].rect())
-        led.value(1)
-    else:
-        led.value(0)
-    img.draw_string(0, 200, 'LED=' + str(led.value()), scale=2)
+while(True):                                        # 永久ループ
+    img = sensor.snapshot()                         # 撮影した写真をimgに代入
+    objects = kpu.run_yolo2(task, img)              # 写真img内の顔検出を実行
+    if objects:                                     # 1件以上検出したとき
+        img.draw_rectangle(objects[0].rect())       # 1件目の検出範囲をimgに追記
+        led_r.value(led_stat.index('On'))           # LEDを点灯(GPIOをLレベルに)
+    else:                                           # 検出しなかったとき
+        led_r.value(led_stat.index('Off'))          # LEDを消灯(GPIOをHレベルに)
+    img.draw_string(0,210,'LED='+led_stat[led_r.value()],scale=3) # 件数をimgに
+    lcd.display(img)                                # imgをLCDに表示
+
+################################################################################
+# 参考文献
+################################################################################
+#・demo_find_face.py
+# (https://github.com/sipeed/MaixPy_scripts/blob/master/machine_vision/face_find/)
+#・demo_gpio_led.py
+# (https://github.com/sipeed/MaixPy_scripts/blob/master/hardware/)
+#・GPIO情報
+# (https://wiki.sipeed.com/soft/maixpy/en/api_reference/Maix/gpio.html)
+# (https://wiki.sipeed.com/soft/maixpy/en/api_reference/Maix/fpioa.html)
+# (https://github.com/sipeed/MaixPy_scripts/blob/master/board/config_maix_dock.py)
